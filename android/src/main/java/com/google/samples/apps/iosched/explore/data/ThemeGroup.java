77360cf300b7904e4fdb833daedd5da19ccbeb8e
Eric Wei <wnlvcq@gmail.com>
2015-11-28 21:20:46 -0800

    change main project folder

--- android/src/main/java/com/google/samples/apps/iosched/explore/data/ThemeGroup.java
+++
@@ -1,7 +0,0 @@
-package com.jjcamera.apps.iosched.explore.data;
-
-/**
- * Data describing a single Theme item to be displayed in a Theme Group on the Explore screen.
- */
-public class ThemeGroup extends ItemGroup {
-}
